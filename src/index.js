fa4a4c7b35542d8d6ac828ab6042d260d1ffedd5
George Zisis <georgezisis@gmail.com>
2025-05-24 10:40:02 +0300

    Add custom cursor, add cursor animation, add cursor color change, add hover effects on header and footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+
+const cursorDot = document.querySelector(".cursor-dot")
+const cursorOutline = document.querySelector(".cursor-outline")
+
+window.addEventListener("mousemove", (event) => {
+    const posX = event.clientX
+    const posY = event.clientY
+
+    cursorDot.style.left = `${posX}px`
+    cursorDot.style.top = `${posY}px`
+
+    cursorOutline.style.left = `${posX}px`
+    cursorOutline.style.top = `${posY}px`
+
+    cursorOutline.animate(
+        {
+            left: `${posX}px`,
+            top: `${posY}px`,
+        },
+        {
+            duration: 500,
+            fill: "backwards"
+        }
+    )
+
+    if (event.target.nodeName === "BUTTON") {
+        cursorDot.classList.add("active")
+        cursorOutline.classList.add("active")
+    } else {
+        cursorDot.classList.remove("active")
+        cursorOutline.classList.remove("active")
+    }
+})
