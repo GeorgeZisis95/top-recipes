57a9d42a7a58fabf10ae1d345fcc185a81cf5ba5
George Zisis <georgezisis@gmail.com>
2025-05-25 10:32:38 +0300

    Add data type for index to recipe container

--- src/toggle_view.js
+++ src/toggle_view.js
@@ -1,7 +1,7 @@
 function viewRecipe(index) {
     const container = document.querySelector("main .container")
     container.classList.add("hide")
-    const recipe = document.querySelector(`.recipe-container-${index}`)
+    const recipe = document.querySelector(`[data-number="${index}"]`)
     recipe.classList.remove("hide")
 
     const resets = Array.from(document.querySelectorAll(".reset-button"))
